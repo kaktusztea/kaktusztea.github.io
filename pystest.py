6c49b695309dc4b9f3dab7311926d5a1abf5a158
kaktusztea <kaktusztea@users.noreply.github.com>
2023-04-19 13:30:28 +0200

    Create pystest.py

---
+++ pystest.py
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <link rel="stylesheet" href="https://pyscript.net/latest/pyscript.css" />
+    <script defer src="https://pyscript.net/latest/pyscript.js"></script>
+</head>
+
+<body>
+
+    <h1>My First Heading</h1>
+    <p>My first paragraph.</p>
+
+    <py-script> print('Hello, World!') </py-script>
+
+</body>
+
+</html>
